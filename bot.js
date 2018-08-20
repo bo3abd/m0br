42309e4e83c52f66e1b396d9d4025f6d4f5bfe03
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-20 09:19:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ console.log(`[M] ${client.users.size}`)
 });
 
 
-const prefix = "♤"
+const prefix = "+"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(prefix)) return undefined;
@@ -212,8 +212,8 @@ function play(guild, song) {
 
 
 
-const adminprefix = "♤";
-const devs = ['301422996562771978','472052319006228509'];
+const adminprefix = "+";
+const devs = ['472052319006228509'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
